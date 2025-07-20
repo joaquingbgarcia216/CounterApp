a090ff0230a6288921e7b97342be9c112ad61945
Joaquin Garcia <joaquing@myvcs.ca>
2025-07-20 00:11:51 -0700

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Joaquin Garcia on 2025-07-20.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CounterAppTests/CounterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppTests.swift
+//  CounterAppTests
+//
+//  Created by Joaquin Garcia on 2025-07-20.
+//
+
+import Testing
+@testable import CounterApp
+
+struct CounterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
